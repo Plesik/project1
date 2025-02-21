6561dfc4c478a5883a8252a5c91b505913c2a3e1
Данил Плес <daniil.plesovskiy@gmail.com>
2025-02-21 17:17:56 +0300

    Initial commit: CMake project setup

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+
+int main() {
+    int x = 5, y = 3;
+    std::cout << "This is an updated message in the main branch!" << std::endl;
+    std::cout << "Sum: " << add(x, y) << std::endl;
+    std::cout << "Difference: " << subtract(x, y) << std::endl;
+    std::cout << "Product: " << multiply(x, y) << std::endl;
+    return 0;
+}
